b7be1795ea9d0e637670ae6b8c79115e2a442437
KosmicDaKerbal <ankurparchure@gmail.com>
2025-04-10 10:58:39 +0530

    help section update

--- faucet-bot/src/commands/help.js
+++ faucet-bot/src/commands/help.js
@@ -3,6 +3,9 @@ const process = require("process");
 module.exports = {
   send: async function (embed) {
     const help = new EmbedBuilder().setTitle("Help Section").setColor(0xf18701).addFields(
+      {
+        name: "Faucet Related Commands:",
+      },
         {
           name: "/help",
           value: "Complete Commands List for the Bot.",
@@ -44,12 +47,58 @@ module.exports = {
           inline: true,
         },
         {
+          name: "Fun",
+        },
+        {
+          name: "/buzzwords",
+          value:
+            "Generates a bunch of corporate marketing bullshit",
+          inline: true,
+        },
+        {
+          name: "/comedy",
+          value:
+            "Tells a joke",
+          inline: true,
+        },
+        {
+          name: "/eyebleach",
+          value:
+            "Gives a random image of a cat, dog, duck or fox.",
+          inline: true,
+        },
+        {
+          name: "/insultme",
+          value:
+            "Insults you",
+          inline: true,
+        },
+        {
+          name: "/kanye",
+          value:
+            "Gives a random quote from Kanye West's tweets",
+          inline: true,
+        },
+        {
+          name: "/quote",
+          value:
+            "Gives a motivational quote by a great personality",
+          inline: true,
+        },
+        {
+          name: "/waifu",
+          value:
+            "Gives a random image of a waifu/husbando",
+          inline: true,
+        },
+        /*
+        {
           name: "Donate",
           value:
             "Support me by Donating to my BAN wallet (It's easier for transactions). Even a small amount would suffice.",
-        },
+        },*/
       )
-      .setImage(process.env.DONATE)
+      //.setImage(process.env.DONATE)
       .setFooter({ text: `v${process.env.BOT_VERSION}`, iconURL: process.env.ICON })
       .setTimestamp();
     await embed.reply({ embeds: [help] });
