c468e9090b4d0f338bd886d5ecfa4580bcd60680
KosmicDaKerbal <ankurparchure@gmail.com>
2026-07-25 13:51:57 +0530

    add bot start separator

--- faucet-bot/src/index.js
+++ faucet-bot/src/index.js
@@ -5,6 +5,7 @@ const https = require("https");
 const fs = require("fs")
 const path = require("path");
 const errorHandler = require(__dirname + "/errorHandler");
+errorHandler.log("============================[APPLICATION BOOT]============================");
 const pkg = require(__dirname + "/../package.json");
 const diagnosticsChannel = require("diagnostics_channel");
 const querylogger = diagnosticsChannel.tracingChannel('mysql2:query');
